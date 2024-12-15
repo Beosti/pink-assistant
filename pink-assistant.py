26d453b866b7b018e2fef228e877ce28e8170816
beosti <beostishi@gmail.com>
2024-12-15 21:25:55 +0100

    can now delete lines

--- pink-assistant.py
+++ pink-assistant.py
@@ -10,12 +10,28 @@ if file_path_todofile:
     content = todofile.read()
 
     parts = content.split("#")
+    
+    parts_map = {}
 
-    for part in parts:
+    for index, part in enumerate(parts, start=1):
         stripped_part = part.strip()
+        parts_map[index] = stripped_part
         if stripped_part:
             print(f"- {stripped_part}")
-
+    
+    print("To delete a todo, just press the number of the part:")
+    part_number = input()
+    part_number = int (part_number)
+    if part_number in parts_map:
+        print(f"Deleted the to-do {part_number}")
+        with open(file_path_todofile, "r") as todofile:
+            lines = todofile.readlines()
+        del lines[part_number - 1]
+        with open(file_path_todofile, "w") as file:
+            file.writelines(lines)
+    else:
+        print(parts_map)
+        print(f"No number {part_number} in the to-do")
     todofile.close()
 else:
     print("TEXT_FILE_PATH is not set in the .env file")
