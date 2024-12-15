e59ff3a34b7d181b7d97ecd608445aeba799f752
beosti <beostishi@gmail.com>
2024-12-15 21:29:23 +0100

    added option to delete or not todo

--- pink-assistant.py
+++ pink-assistant.py
@@ -18,20 +18,25 @@ if file_path_todofile:
         parts_map[index] = stripped_part
         if stripped_part:
             print(f"- {stripped_part}")
-    
-    print("To delete a todo, just press the number of the part:")
-    part_number = input()
-    part_number = int (part_number)
-    if part_number in parts_map:
-        print(f"Deleted the to-do {part_number}")
-        with open(file_path_todofile, "r") as todofile:
-            lines = todofile.readlines()
-        del lines[part_number - 1]
-        with open(file_path_todofile, "w") as file:
-            file.writelines(lines)
+   
+    print("Do you want to delete a todo?: y/n")
+    delete = input()
+    if delete == "y":
+        print("To delete a todo, just press the number of the part:")
+        part_number = input()
+        part_number = int (part_number)
+        if part_number in parts_map:
+            print(f"Deleted the to-do {part_number}")
+            with open(file_path_todofile, "r") as todofile:
+                lines = todofile.readlines()
+            del lines[part_number - 1]
+            with open(file_path_todofile, "w") as file:
+                file.writelines(lines)
+        else:
+            print(parts_map)
+            print(f"No number {part_number} in the to-do")
     else:
-        print(parts_map)
-        print(f"No number {part_number} in the to-do")
+        print("good luck sir!")
     todofile.close()
 else:
     print("TEXT_FILE_PATH is not set in the .env file")
