fe451186547331a3b40142604ede8b6b087c29c4
beosti <beostishi@gmail.com>
2024-12-16 14:39:51 +0100

    added options

--- pink-assistant.py
+++ pink-assistant.py
@@ -19,9 +19,9 @@ if file_path_todofile:
         if stripped_part:
             print(f"- {stripped_part}")
    
-    print("Do you want to delete a todo?: y/n")
-    delete = input()
-    if delete == "y":
+    print("Do you want to (a)dd/(d)elete/(n)othing to the todo?: ")
+    input_action = input()
+    if input_action == "d":
         print("To delete a todo, just press the number of the part:")
         part_number = input()
         part_number = int (part_number)
@@ -35,8 +35,18 @@ if file_path_todofile:
         else:
             print(parts_map)
             print(f"No number {part_number} in the to-do")
-    else:
+    elif input_action == "a":
+        print("What to-do would you like to add: ")
+        input_addition = input()
+        with open(file_path_todofile, "r") as file:
+            lines = file.readlines()
+        lines.append("\n# " + input_addition)
+        with open(file_path_todofile, "w") as todofile:
+            todofile.writelines(lines)
+    elif input_action == "n":
         print("good luck sir!")
+    else:
+        print("not a good input!")
     todofile.close()
 else:
     print("TEXT_FILE_PATH is not set in the .env file")
